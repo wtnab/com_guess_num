cce1e0bd621e30181c4eae08391713126a2c49be
wtnab <ans42r@gmail.com>
2017-08-20 20:05:57 +0900

    no message

---
+++ python/test/list.py
@@ -0,0 +1,7 @@
+list = []
+list = range(10)
+print list
+
+print len(list)
+
+print list[2]
---
+++ python/test/range.py
@@ -0,0 +1,8 @@
+print range(10)
+print range(4,9)
+
+list = []
+for i in range(2345,2350):
+    list = list + [i]
+
+print list
